809af3c6d13d74efbb6e373bc0fc01fc721cf299
程飞 <chengfei@newland.com.cn>
2022-03-21 16:59:20 +0800

    Scala相关

---
+++ scala-code/src/main/scala/com/feifei/base/Test.java
@@ -0,0 +1,13 @@
+package com.feifei.base;
+
+/**
+ * @Description:
+ * @ClassName: Test
+ * @Author chengfei
+ * @DateTime 2022/3/21 16:14
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("fds");
+    }
+}
